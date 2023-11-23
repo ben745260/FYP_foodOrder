be59c51299baa2750510f2399c9d7397371fb714
Ben <106643280+ben745260@users.noreply.github.com>
2023-11-23 14:03:28 +0800

    Updating menus

--- FrontEnd/src/components/Menus.vue
+++ FrontEnd/src/components/Menus.vue
@@ -1,71 +1,82 @@
 <template>
-    <div>
-      <h1>All Products</h1>
-      <div class="menu-container">
-        <div v-for="product in products" :key="product.product_id" class="menu-item">
-            <img class="menu-image" :src="product.image" alt="Product Image">
-          <div class="menu-details">
-            <h3>{{ product.product_name }}</h3>
-            <p>Price: {{ product.price }}</p>
-            <p>Category: {{ product.category }}</p>
-            <p>Published Date: {{ product.pub_date }}</p>
-          </div>
+  <div>
+    <h1>All Products</h1>
+    <div class="menu-container">
+      <div v-for="product in products" :key="product.product_id" class="menu-item">
+        <img class="menu-image" :src="product.image" alt="Product Image">
+        <div class="menu-details">
+          <h3>{{ product.product_name }}</h3>
+          <p>Price: $&nbsp;{{ product.price }}</p>
+          <p>Category: {{ getCategoryLabel(product.category) }}</p>
+          <p>Published Date: {{ product.pub_date }}</p>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  import apiClient from '@/axios/apiClient';
-  
-  export default {
-    name: 'Menus',
-    data() {
-      return {
-        products: []
-      };
-    },
-    mounted() {
-      apiClient.get('/products/')
-        .then(response => {
-          this.products = response.data;
-        })
-        .catch(error => {
-          console.error(error);
-        });
+<script>
+import apiClient from '@/axios/apiClient';
+
+export default {
+  name: 'Menus',
+  data() {
+    return {
+      products: []
+    };
+  },
+  mounted() {
+    apiClient.get('/products/')
+      .then(response => {
+        this.products = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+  methods: {
+    getCategoryLabel(category) {
+      if (category =='1') {
+        return 'Fruit';
+      } else if (category == '2') {
+        return 'Drink';
+      } else {
+        return 'Unknown';
+      }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .menu-container {
-    display: flex;
-    flex-wrap: wrap;
   }
+};
+</script>
   
-  .menu-item {
-    margin: 20px;
-    padding: 20px;
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    display: flex;
-  }
-  
-  .menu-image {
-    width: 150px;
-    height: 150px;
-    margin-right: 20px;
-  }
-  
-  .menu-details {
-    flex-grow: 1;
-  }
-  
-  .menu-details h3 {
-    margin-top: 0;
-  }
-  
-  .menu-details p {
-    margin-bottom: 5px;
-  }
-  </style>
+<style scoped>
+.menu-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.menu-item {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  display: flex;
+}
+
+.menu-image {
+  width: 150px;
+  height: 150px;
+  margin-right: 20px;
+}
+
+.menu-details {
+  flex-grow: 1;
+}
+
+.menu-details h3 {
+  margin-top: 0;
+}
+
+.menu-details p {
+  margin-bottom: 5px;
+}
+</style>
