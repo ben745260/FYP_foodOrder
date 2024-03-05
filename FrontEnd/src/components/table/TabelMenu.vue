b758bfec183d1c652e7f6d292ec508242b77c004
ben745260 <106643280+ben745260@users.noreply.github.com>
2024-03-05 18:48:48 +0800

    WIP products in menu

--- FrontEnd/src/components/table/TabelMenu.vue
+++ FrontEnd/src/components/table/TabelMenu.vue
@@ -1,86 +1,104 @@
 <template>
     <v-app>
-        <v-row>
-            <v-col cols="4">
-                <v-card class="h-100" style="overflow-y: scroll;">
-                    <v-list>
-                        <v-list-item v-for="category in categories" :key="category.category_id" link
-                            @click="scrollToCard(category.name)">
-                            <v-list-item-content class="pl-0">
-                                {{ category.name }}
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-card>
+      <v-row>
+        <v-col cols="4">
+          <v-card class="h-100" style="overflow-y: scroll;">
+            <v-list>
+              <v-list-item v-for="category in categories" :key="category.category_id" link @click="scrollToCard(category.category_id)">
+                <v-list-item-content class="pl-0">
+                  {{ category.name }}
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
+        <v-col cols="8">
+          <v-row>
+            <v-col v-for="category in categories" :key="category.category_id" cols="12" class="ps-1">
+              <v-card :id="`card-${category.category_id}`" variant="flat">
+                <v-card-title>{{ category.name }}</v-card-title>
+                <v-card-text>
+                  <ul>
+                    <li v-for="product in getProductsByCategory(category.category_id)" :key="product.product_id">
+                      {{ product.product_name }}
+                    </li>
+                  </ul>
+                </v-card-text>
+              </v-card>
             </v-col>
-            <v-col cols="8">
-                <v-row>
-                    <v-col v-for="category in categories" :key="category.category_id" cols="12" class="ps-1">
-                        <v-card :id="category.name" variant="flat">
-                            <v-card-title>{{ category.name }}</v-card-title>
-                            <v-card-text>
-                                <!-- Content of the card -->
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
+          </v-row>
+        </v-col>
+      </v-row>
     </v-app>
-</template>
-
-<script>
-import apiClient from '@/axios/apiClient';
-
-export default {
+  </template>
+  
+  <script>
+  import apiClient from '@/axios/apiClient';
+  
+  export default {
     name: "TabelMenu",
     data() {
-        return {
-            drawer: false,
-            categories: [],
-        };
+      return {
+        drawer: false,
+        categories: [],
+        products: [],
+      };
     },
     mounted() {
-        this.fetchCategories();
+      this.fetchCategories();
+      this.fetchProducts();
     },
     methods: {
-        fetchCategories() {
-            apiClient
-                .get('/productcategories/')
-                .then((response) => {
-                    const categories = response.data.map((category) => ({
-                        category_id: category.category_id,
-                        name: category.name,
-                        icon: category.icon,
-                    }));
-                    this.categories = categories;
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        },
-        scrollToCard(categoryName) {
-            const cardElement = document.getElementById(categoryName);
-            if (cardElement) {
-                cardElement.scrollIntoView({ behavior: 'smooth' });
-            }
-        },
+      fetchCategories() {
+        apiClient
+          .get('/productcategories/')
+          .then((response) => {
+            const categories = response.data.map((category) => ({
+              category_id: category.category_id,
+              name: category.name,
+              icon: category.icon,
+            }));
+            this.categories = categories;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
+      fetchProducts() {
+        apiClient
+          .get('/products/')
+          .then((response) => {
+            this.products = response.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
+      scrollToCard(categoryId) {
+        const cardElement = document.getElementById(`card-${categoryId}`);
+        if (cardElement) {
+          cardElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      },
+      getProductsByCategory(categoryId) {
+        return this.products.filter((product) => product.category === categoryId);
+      },
     },
-};
-</script>
-
-<style scoped>
-/* .v-card{
-      margin-bottom: 1;
-  } */
-.v-row {
+  };
+  </script>
+  
+  <style scoped>
+  /* .v-card{
+        margin-bottom: 1;
+    } */
+  .v-row {
     /* padding-left: 0; */
     padding-right: 0;
-}
-
-.v-col {
+  }
+  
+  .v-col {
     /* padding-left: 0; */
     padding-right: 0;
     padding-bottom: 0;
-}
-</style>
+  }
+  </style>
