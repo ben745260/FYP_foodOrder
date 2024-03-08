38c1ce298dde5e0d9cd0fd305ff027e132bd2026
ben745260 <106643280+ben745260@users.noreply.github.com>
2024-03-08 22:45:28 +0800

    Update addtoCart()

--- FrontEnd/src/components/table/ProductDetail.vue
+++ FrontEnd/src/components/table/ProductDetail.vue
@@ -1,128 +1,155 @@
 <template>
-    <v-dialog
-      v-model="dialogVisible"
-      fullscreen
-      hide-overlay
-      transition="dialog-bottom-transition"
-    >
-      <v-toolbar color="grey lighten-3">
-        <v-btn @click="closeDialog"
-          ><i class="fa-solid fa-xmark fa-2xl"></i
-        ></v-btn>
-        <v-toolbar-title class="fs-3">{{
-          selectedProduct ? selectedProduct.product_name : ""
-        }}</v-toolbar-title>
-      </v-toolbar>
-      <v-card>
-        <v-card-title class="p-0">
-          <v-tabs v-model="activeTab" background-color="transparent" grow>
-            <v-tab value="detail">Detail</v-tab>
-            <v-tab value="qna">QnA</v-tab>
-          </v-tabs>
-        </v-card-title>
-        <v-card-text>
-          <v-window v-model="activeTab">
-            <v-window-item value="detail">
-              <v-img
-                :src="selectedProduct ? selectedProduct.image : ''"
-                class="rounded border border-2 border-danger"
-              />
-              <div class="mt-4 fs-5">
-                <strong>Desciption:</strong>
-                <div
-                  v-html="selectedProduct ? selectedProduct.product_detail : ''"
-                ></div>
-              </div>
-            </v-window-item>
-            <v-window-item value="qna"> This is the QnA tab. </v-window-item>
-          </v-window>
-        </v-card-text>
-      </v-card>
-      <v-footer class="p-0" app>
-        <v-toolbar dark>
-          <v-toolbar-title class="fs-4 fw-bold ps-3">
-            ${{ selectedProduct ? selectedProduct.price : "" }}
-          </v-toolbar-title>
-          <div class="mx-auto">
-            <v-btn
-              :variant="'elevated'"
-              color="primary"
-              @click="decrementQuantity"
-              size="small"
-            >
-              <v-icon>fa-solid fa-minus</v-icon>
-            </v-btn>
-            <span class="badge text-bg-light fs-6 my-auto mx-2">{{
-              quantity
-            }}</span>
-            <v-btn
-              :variant="'elevated'"
-              color="primary"
-              @click="incrementQuantity"
-              size="small"
-            >
-              <v-icon>fa-solid fa-plus</v-icon>
-            </v-btn>
-          </div>
+  <v-dialog
+    v-model="dialogVisible"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <v-toolbar color="grey lighten-3">
+      <v-btn @click="closeDialog"
+        ><i class="fa-solid fa-xmark fa-2xl"></i
+      ></v-btn>
+      <v-toolbar-title class="fs-3">{{
+        selectedProduct ? selectedProduct.product_name : ""
+      }}</v-toolbar-title>
+    </v-toolbar>
+    <v-card>
+      <v-card-title class="p-0">
+        <v-tabs v-model="activeTab" background-color="transparent" grow>
+          <v-tab value="detail">Detail</v-tab>
+          <v-tab value="qna">QnA</v-tab>
+        </v-tabs>
+      </v-card-title>
+      <v-card-text>
+        <v-window v-model="activeTab">
+          <v-window-item value="detail">
+            <v-img
+              :src="selectedProduct ? selectedProduct.image : ''"
+              class="rounded border border-2 border-danger"
+            />
+            <div class="mt-4 fs-5">
+              <strong>Desciption:</strong>
+              <div
+                v-html="selectedProduct ? selectedProduct.product_detail : ''"
+              ></div>
+            </div>
+          </v-window-item>
+          <v-window-item value="qna"> This is the QnA tab. </v-window-item>
+        </v-window>
+      </v-card-text>
+    </v-card>
+    <v-footer class="p-0" app>
+      <v-toolbar dark>
+        <v-toolbar-title class="fs-4 fw-bold ps-3">
+          ${{ selectedProduct ? selectedProduct.price : "" }}
+        </v-toolbar-title>
+        <div class="mx-auto">
           <v-btn
-            :variant="'outlined'"
+            :variant="'elevated'"
             color="primary"
-            @click="addToCart"
-            rounded="xl"
-            class="ms-1"
+            @click="decrementQuantity"
+            size="small"
           >
-            Add to Cart
+            <v-icon>fa-solid fa-minus</v-icon>
           </v-btn>
-        </v-toolbar>
-      </v-footer>
-    </v-dialog>
-  </template>
+          <span class="badge text-bg-light fs-6 my-auto mx-2">{{
+            quantity
+          }}</span>
+          <v-btn
+            :variant="'elevated'"
+            color="primary"
+            @click="incrementQuantity"
+            size="small"
+          >
+            <v-icon>fa-solid fa-plus</v-icon>
+          </v-btn>
+        </div>
+        <v-btn
+          :variant="'outlined'"
+          color="primary"
+          @click="addToCart"
+          rounded="xl"
+          class="ms-1"
+        >
+          Add to Cart
+        </v-btn>
+      </v-toolbar>
+    </v-footer>
+  </v-dialog>
+</template>
   
   <script>
-  export default {
-    name: "DialogComponent",
-    props: {
-      selectedProduct: {
-        type: Object,
-        default: null,
-      },
+export default {
+  name: "DialogComponent",
+  props: {
+    selectedProduct: {
+      type: Object,
+      default: null,
     },
-    data() {
-      return {
-        dialogVisible: false,
-        quantity: 0,
-        activeTab: "detail", // Set the default active tab to 'detail'
-      };
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      quantity: 0,
+      activeTab: "detail", // Set the default active tab to 'detail'
+    };
+  },
+  watch: {
+    dialogVisible(value) {
+      if (value) {
+        this.activeTab = "detail"; // Reset active tab to 'detail'
+        this.quantity= 0;
+      }
     },
-    watch: {
-      dialogVisible(value) {
-        if (value) {
-          this.activeTab = "detail"; // Reset active tab to 'detail'
+  },
+  methods: {
+    closeDialog() {
+      this.dialogVisible = false;
+    },
+    incrementQuantity() {
+      this.quantity++;
+    },
+    decrementQuantity() {
+      if (this.quantity > 0) {
+        this.quantity--;
+      }
+    },
+    addToCart() {
+      // Check if the selectedProduct and quantity are valid
+      if (this.selectedProduct && this.quantity > 0) {
+        // Get the existing cart items from the local storage
+        const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+        // Check if the selected product is already in the cart
+        const existingCartItem = cartItems.find(
+          (item) => item.productId === this.selectedProduct.id
+        );
+
+        if (existingCartItem) {
+          // If the product already exists in the cart, update its quantity
+          existingCartItem.quantity += this.quantity;
+        } else {
+          // If the product does not exist in the cart, add it as a new item
+          cartItems.push({
+            productId: this.selectedProduct.id,
+            quantity: this.quantity,
+          });
         }
-      },
+
+        // Save the updated cart items to the local storage
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+        // Close the dialog
+        this.closeDialog();
+      }
     },
-    methods: {
-      closeDialog() {
-        this.dialogVisible = false;
-      },
-      incrementQuantity() {
-        this.quantity++;
-      },
-      decrementQuantity() {
-        if (this.quantity > 0) {
-          this.quantity--;
-        }
-      },
-      addToCart() {
-        // Add your logic for adding the product to the cart here
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
   
   <style scoped>
-  .square-bg-color {
-    background-color: white;
-    border-radius: 0;
-  }
-  </style>
+.square-bg-color {
+  background-color: white;
+  border-radius: 0;
+}
+</style>
