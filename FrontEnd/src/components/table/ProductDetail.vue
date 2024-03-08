a820508712c6f60bd2bad5c931574849885643cc
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-08 16:34:01 +0800

    Add product detail layout

---
+++ FrontEnd/src/components/table/ProductDetail.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-dialog
+    v-model="dialogVisible"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <v-toolbar color="grey lighten-3">
+      <v-btn @click="closeDialog"
+        ><i class="fa-solid fa-xmark fa-2xl"></i
+      ></v-btn>
+      <v-toolbar-title class="fs-3">{{
+        selectedProduct ? selectedProduct.product_name : ""
+      }}</v-toolbar-title>
+    </v-toolbar>
+    <v-card>
+      <v-card-title>
+        <v-tabs v-model="activeTab" background-color="transparent" grow>
+          <v-tab value="detail">Detail</v-tab>
+          <v-tab value="qna">QnA</v-tab>
+        </v-tabs>
+      </v-card-title>
+      <v-card-text>
+        <v-window v-model="activeTab">
+          <v-window-item value="detail">
+            <v-img
+              :src="selectedProduct ? selectedProduct.image : ''"
+              class="rounded border border-2 border-danger"
+            />
+            <div class="mt-4 fs-5">
+              <strong>Desciption:</strong>
+              <div
+                v-html="selectedProduct ? selectedProduct.product_detail : ''"
+              ></div>
+            </div>
+          </v-window-item>
+          <v-window-item value="qna"> This is the QnA tab. </v-window-item>
+        </v-window>
+      </v-card-text>
+    </v-card>
+    <v-footer class="p-0" app>
+      <v-toolbar dark>
+        <v-toolbar-title class="fs-4 fw-bold ps-3">
+          ${{ selectedProduct ? selectedProduct.price : "" }}
+        </v-toolbar-title>
+        <div class="mx-auto">
+          <v-btn
+            :variant="'elevated'"
+            color="primary"
+            @click="decrementQuantity"
+            size="small"
+          >
+            <v-icon>fa-solid fa-minus</v-icon>
+          </v-btn>
+          <span class="badge text-bg-light fs-6 my-auto mx-2">{{
+            quantity
+          }}</span>
+          <v-btn
+            :variant="'elevated'"
+            color="primary"
+            @click="incrementQuantity"
+            size="small"
+          >
+            <v-icon>fa-solid fa-plus</v-icon>
+          </v-btn>
+        </div>
+        <v-btn
+          :variant="'outlined'"
+          color="primary"
+          @click="addToCart"
+          rounded="xl"
+          class="ms-1"
+        >
+          Add to Cart
+        </v-btn>
+      </v-toolbar>
+    </v-footer>
+  </v-dialog>
+</template>
+  
+  <script>
+export default {
+  name: "DialogComponent",
+  props: {
+    selectedProduct: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      quantity: 0,
+      activeTab: "detail", // Set the default active tab to 'detail'
+    };
+  },
+  methods: {
+    closeDialog() {
+      this.dialogVisible = false;
+    },
+    incrementQuantity() {
+      this.quantity++;
+    },
+    decrementQuantity() {
+      if (this.quantity > 0) {
+        this.quantity--;
+      }
+    },
+    addToCart() {
+      // Add your logic for adding the product to the cart here
+    },
+  },
+};
+</script>
+  
+  <style scoped>
+.square-bg-color {
+  background-color: white;
+  border-radius: 0;
+}
+</style>
--- FrontEnd/src/components/table/TabelMenu.vue
+++ FrontEnd/src/components/table/TabelMenu.vue
@@ -1,24 +1,6 @@
 <template>
   <v-app>
-    <!-- <v-row>
-      <v-col cols="4" class="py-0">
-        <v-card class="h-100 w-25" style="position: fixed">
-          <v-list>
-            <v-list-item
-              v-for="category in categories"
-              :key="category.category_id"
-              link
-              @click="scrollToCard(category.category_id)"
-            >
-              <v-list-item-content class="pl-0">
-                {{ category.name }}
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
-      <v-col cols="8" class="py-0 ps-0"> -->
-    <v-container class="pt-0 ps-1">
+    <v-container class="pt-1 px-1">
       <v-row>
         <v-col
           v-for="category in categories"
@@ -27,46 +9,55 @@
           class="ps-1 pt-0"
         >
           <v-card :id="`card-${category.category_id}`" variant="flat">
-            <v-card-title>{{ category.name }} </v-card-title>
+            <v-card-title class="fs-4">{{ category.name }}</v-card-title>
             <v-card-text>
               <v-row
                 v-for="product in getProductsByCategory(category.category_id)"
+                @click="openDialog(product)"
                 :key="product.product_id"
               >
                 <v-col cols="4">
                   <v-img
                     :src="product.image"
                     :height="100"
-                    class="round-sm border border-primary"
+                    class="rounded border border-primary"
                     alt="Product Image"
                   ></v-img>
                 </v-col>
-                <v-col cols="6">
-                  <h4>{{ product.product_name }}</h4>
-                  <p>${{ product.price }}</p>
+                <v-col cols="8">
+                  <h3>{{ product.product_name }}</h3>
+                  <h3 class="text-primary mt-5 pt-3">
+                    ${{ product.price }}
+                    <v-btn class="float-end fa-solid fa-plus"></v-btn>
+                  </h3>
                 </v-col>
-                <v-col cols="2"></v-col>
               </v-row>
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
+    <dialog-component :selectedProduct="selectedProduct" ref="dialog"></dialog-component>
     <!-- </v-col>
     </v-row> -->
   </v-app>
 </template>
-  
-  <script>
+
+<script>
 import apiClient from "@/axios/apiClient";
+import DialogComponent from "./ProductDetail.vue";
 
 export default {
   name: "TabelMenu",
+  components: {
+    DialogComponent,
+  },
   data() {
     return {
       drawer: false,
       categories: [],
       products: [],
+      selectedProduct: null, // Add selectedProduct property
     };
   },
   mounted() {
@@ -102,11 +93,15 @@ export default {
     getProductsByCategory(categoryId) {
       return this.products.filter((product) => product.category === categoryId);
     },
+    openDialog(product) {
+      this.selectedProduct = product;
+      this.$refs.dialog.dialogVisible = true;
+    },
   },
 };
 </script>
-  
-  <style scoped>
+
+<style scoped>
 /* .v-card{
         margin-bottom: 1;
     } */
@@ -117,7 +112,7 @@ export default {
 
 .v-col {
   /* padding-left: 0; */
-  padding-right: 0;
-  padding-bottom: 0;
+  /* padding-right: 0; */
+  /* padding-bottom: 0; */
 }
 </style>
