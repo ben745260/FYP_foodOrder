96a045b4d5c8c9c84cdb6d460211382957a43f0b
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-09 15:15:19 +0800

    WIP Checkout

--- FrontEnd/src/components/table/Cart.vue
+++ FrontEnd/src/components/table/Cart.vue
@@ -1,124 +1,126 @@
 <template>
-    <v-app>
-      <v-container>
-        <v-card>
-          <v-card-title>
-            My Cart ({{ cartItems.length }} items)
-            <v-btn variant="tonal" color="error" @click="confirmClearItems" style="float: right;">Clear all items</v-btn>
-          </v-card-title>
-          <v-card-text>
-            <v-table>
-              <thead>
-                <tr>
-                  <th class="text-left">Name</th>
-                  <th class="text-left">Quantity</th>
-                  <th class="text-left">Price</th>
-                  <th class="text-left"></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in cartItems" :key="item.productId">
-                  <td class="product-name">
-                    <strong>{{ getProductById(item.productId)?.product_name }}</strong>
-                  </td>
-                  <td>{{ item.quantity }}</td>
-                  <td>
-                    $&nbsp;{{
-                      item.quantity * getProductById(item.productId)?.price
-                    }}
-                  </td>
-                  <td>
-                    <button @click="removeItem(item.productId)">
-                      <i class="fa-solid fa-trash" style="color:#B00020"></i>
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </v-table>
-          </v-card-text>
-        </v-card>
-        
-        <!-- Confirmation Dialog -->
-        <v-dialog v-model="confirmDialog" max-width="500px">
-          <v-card>
-            <v-card-title class="headline">Clear All Items</v-card-title>
-            <v-card-text>
-              <span>Are you sure you want to clear all items from your cart?</span>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="error" text @click="clearAllItems">Confirm</v-btn>
-              <v-btn text @click="confirmDialog = false">Cancel</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-container>
-    </v-app>
-  </template>
-  
-  <script>
-  import apiClient from "@/axios/apiClient";
-  
-  export default {
-    name: "Cart",
-    data() {
-      return {
-        categories: [],
-        products: [],
-        cartItems: this.$store.state.cartItems,
-        confirmDialog: false, // Control variable for the confirmation dialog
-      };
+  <v-app>
+    <v-container>
+      <v-card>
+        <v-card-title>
+          My Cart ({{ cartItems.length }} items)
+          <v-btn variant="tonal" color="error" @click="confirmClearItems" style="float: right;">Clear all items</v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">Name</th>
+                <th class="text-left">Quantity</th>
+                <th class="text-left">Price</th>
+                <th class="text-left"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in cartItems" :key="item.productId">
+                <td class="product-name">
+                  <strong>{{ getProductById(item.productId)?.product_name }}</strong>
+                </td>
+                <td>{{ item.quantity }}</td>
+                <td>
+                  $&nbsp;{{
+                    item.quantity * getProductById(item.productId)?.price
+                  }}
+                </td>
+                <td>
+                  <button @click="removeItem(item.productId)">
+                    <i class="fa-solid fa-trash" style="color:#B00020"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-card-text>
+      </v-card>
+    </v-container>
+
+
+
+    <!-- Confirmation Dialog -->
+    <v-dialog v-model="confirmDialog" max-width="500px">
+      <v-card>
+        <v-card-title class="headline">Clear All Items</v-card-title>
+        <v-card-text>
+          <span>Are you sure you want to clear all items from your cart?</span>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn color="error" text @click="clearAllItems">Confirm</v-btn>
+          <v-btn text @click="confirmDialog = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+import apiClient from "@/axios/apiClient";
+
+export default {
+  name: "Cart",
+  data() {
+    return {
+      categories: [],
+      products: [],
+      cartItems: this.$store.state.cartItems,
+      confirmDialog: false, // Control variable for the confirmation dialog
+    };
+  },
+  mounted() {
+    this.fetchCategories();
+    this.fetchProducts();
+  },
+  methods: {
+    fetchCategories() {
+      apiClient
+        .get("/productcategories/")
+        .then((response) => {
+          const categories = response.data.map((category) => ({
+            category_id: category.category_id,
+            name: category.name,
+            icon: category.icon,
+          }));
+          this.categories = categories;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    mounted() {
-      this.fetchCategories();
-      this.fetchProducts();
+    fetchProducts() {
+      apiClient
+        .get("/products/")
+        .then((response) => {
+          this.products = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    methods: {
-      fetchCategories() {
-        apiClient
-          .get("/productcategories/")
-          .then((response) => {
-            const categories = response.data.map((category) => ({
-              category_id: category.category_id,
-              name: category.name,
-              icon: category.icon,
-            }));
-            this.categories = categories;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-      fetchProducts() {
-        apiClient
-          .get("/products/")
-          .then((response) => {
-            this.products = response.data;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-      getProductById(productId) {
-        return this.products.find((product) => product.product_id === productId);
-      },
-      removeItem(productId) {
-        this.$store.commit("removeItem", productId);
-        this.cartItems = this.$store.state.cartItems; // Fetch the updated cart items
-      },
-      confirmClearItems() {
-        this.confirmDialog = true; // Show the confirmation dialog
-      },
-      clearAllItems() {
-        this.$store.commit("removeAllItems");
-        this.cartItems = this.$store.state.cartItems; // Fetch the updated cart items
-        this.confirmDialog = false; // Close the confirmation dialog
-      },
+    getProductById(productId) {
+      return this.products.find((product) => product.product_id === productId);
     },
-  };
-  </script>
-  
-  <style scoped>
-  .product-name {
-    word-break: break-word;
-  }
-  </style>
+    removeItem(productId) {
+      this.$store.commit("removeItem", productId);
+      this.cartItems = this.$store.state.cartItems; // Fetch the updated cart items
+    },
+    confirmClearItems() {
+      this.confirmDialog = true; // Show the confirmation dialog
+    },
+    clearAllItems() {
+      this.$store.commit("removeAllItems");
+      this.cartItems = this.$store.state.cartItems; // Fetch the updated cart items
+      this.confirmDialog = false; // Close the confirmation dialog
+    },
+  },
+};
+</script>
+
+<style scoped>
+.product-name {
+  word-break: break-word;
+}
+</style>
--- FrontEnd/src/views/Table-User.vue
+++ FrontEnd/src/views/Table-User.vue
@@ -2,13 +2,23 @@
   <v-layout>
     <v-app-bar color="primary" dark>
       <v-toolbar-title class="text-center">
-        <span class="headline fs-2 fw-bolder" @click="$router.push('./tablemenu')">FoodDine</span>
+        <span
+          class="headline fs-2 fw-bolder"
+          @click="$router.push('./tablemenu')"
+          >FoodDine</span
+        >
       </v-toolbar-title>
     </v-app-bar>
 
     <v-app-bar color="light" dense>
       <v-tabs v-model="activeTab" background-color="light" color="primary">
-        <v-tab v-for="tab in tabs" :key="tab.name" :to="tab.to" exact :exact-active-class="'active-link'">
+        <v-tab
+          v-for="tab in tabs"
+          :key="tab.name"
+          :to="tab.to"
+          exact
+          :exact-active-class="'active-link'"
+        >
           {{ tab.label }}
         </v-tab>
       </v-tabs>
@@ -17,6 +27,24 @@
     <v-main class="fixed">
       <router-view></router-view>
     </v-main>
+    
+    <v-footer color="primary" app>
+      <v-btn @click="confirmOrder" variant="text" block class="text-center fs-5">Order</v-btn>
+    </v-footer>
+
+    <!-- Confirmation Dialog -->
+    <v-dialog v-model="confirmDialog" max-width="500px">
+  <v-card>
+    <v-card-title class="headline">Confirm Order</v-card-title>
+    <v-card-text>
+      <span>Are you sure you want to place the order?</span>
+    </v-card-text>
+    <v-card-actions class="justify-end">
+      <v-btn color="primary" text @click="placeOrder">Confirm</v-btn>
+      <v-btn text @click="cancelOrder">Cancel</v-btn>
+    </v-card-actions>
+  </v-card>
+</v-dialog>
   </v-layout>
 </template>
 
@@ -31,13 +59,26 @@ export default {
         { name: "cart", label: "Cart", to: "./cart" },
         { name: "vieworder", label: "Record", to: "./vieworder" },
       ],
+      confirmDialog: false, // Control variable for the confirmation dialog
     };
+  },
+  methods: {
+    confirmOrder() {
+      this.confirmDialog = true; // Show the confirmation dialog
+    },
+    placeOrder() {
+      // Perform your order logic here
+      this.confirmDialog = false; // Close the confirmation dialog
+      this.$router.push("/order");
+    },
+    cancelOrder() {
+      this.confirmDialog = false; // Close the confirmation dialog
+    },
   },
 };
 </script>
 
 <style scoped>
-
 .header-link {
   font-size: 1.5rem;
   color: #fff;
@@ -51,7 +92,7 @@ export default {
 
 .v-main {
   padding-top: 20px;
-  height: 87vh;
+  /* height: 87vh; */
   margin-top: 13vh;
 }
 </style>
