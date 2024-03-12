a793d5ff7659f19792b87ad120ced35e68689f87
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-12 14:52:20 +0800

    Fix vuetify icon

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -15,9 +15,13 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import '@mdi/font/css/materialdesignicons.css'
+
+
 const vuetify = createVuetify({
     components,
     directives,
+    ssr: true,
   })
   
 const app = createApp(App);
