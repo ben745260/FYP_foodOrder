2b4e6901a8c88c316d3ab414a89e23abfa144852
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-12 15:29:48 +0800

    Update admin page nav-drawer

--- FrontEnd/src/views/Admin.vue
+++ FrontEnd/src/views/Admin.vue
@@ -1,57 +1,29 @@
 <template>
-  <div class="admin-panel">
-    <div class="container-fluid h-100">
-      <div class="row h-100">
-        <div class="col-md-2 p-0">
-          <div class="side-bar d-flex flex-column flex-shrink-0 p-3 text-white bg-dark h-100">
-            <router-link to="/admin/dashboard"
-              class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-              <h2 class="fw-bold">FoodDine System</h2>
-            </router-link>
-            <hr>
-            <ul class="nav nav-pills flex-column mb-auto">
-              <li>
-                <router-link to="/admin/dashboard" class="nav-link" active-class="active">
-                  <i class="fa fa-home"></i> Dashboard
-                </router-link>
-              </li>
-              <li>
-                <router-link to="/admin/orders" class="nav-link" active-class="active">
-                  <i class="fa fa-shopping-cart"></i> Orders
-                </router-link>
-              </li>
-              <li>
-                <router-link to="/admin/tables" class="nav-link" active-class="active">
-                  <i class="fa fa-table"></i> Tables
-                </router-link>
-              </li>
-              <li>
-                <router-link to="/admin/menus" class="nav-link" active-class="active">
-                  <i class="fa fa-bars"></i> Menus
-                </router-link>
-              </li>
-            </ul>
-            <hr>
-            <div class="dropdown">
-              <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <span>{{ username }}</span>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                <li><button class="dropdown-item" @click="logout">Sign out</button></li>
-              </ul>
-            </div>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer class="bg-shades" theme="dark" permanent>
+        <v-list color="transparent">
+          <v-list-item><h1>FoodDine<br>System</h1><span class="fw-semibold"> {{ username }}</span></v-list-item>
+          <v-divider></v-divider>
+          <v-list-item link prepend-icon="mdi-view-dashboard" title="" @click="$router.push('/admin/dashboard')"><span class="fw-semibold">Dashboard</span></v-list-item>
+          <v-list-item link prepend-icon="mdi-order-alphabetical-ascending" @click="$router.push('/admin/orders')"><span class="fw-semibold">Orders</span></v-list-item>
+          <v-list-item link prepend-icon="mdi-table" title="" @click="$router.push('/admin/tables')"><span class="fw-semibold" >Tables</span></v-list-item>
+          <v-list-item link prepend-icon="mdi-menu" title="" @click="$router.push('/admin/menus')"><span class="fw-semibold">Menus</span></v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+          <div class="mb-5 px-3">
+            <v-btn class="bg-blue-grey" block @click="logout"> Logout </v-btn>
           </div>
-        </div>
-        <div class="col-md-10 p-0">
-          <div class="bg-body-tertiary h-100 px-5 py-3">
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+        </template>
+      </v-navigation-drawer>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
+
 
 <script>
 export default {
@@ -70,27 +42,4 @@ export default {
 </script>
 
 <style scoped>
-.admin-panel {
-  height: 100vh;
-}
-
-.side-bar {
-  font-weight: 600;
-  color: white;
-}
-
-.nav-link {
-  color: white;
-}
-
-.nav-link i {
-  margin-right: 10px;
-}
-
-.nav-link:hover {
-  background-color: #384454
-}
-
-.nav-link.active {
-  background-color: #384454
-}</style>
+</style>
--- FrontEnd/src/views/Test.vue
+++ FrontEnd/src/views/Test.vue
@@ -1,73 +1,45 @@
 <template>
-  <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">
-          Name
-        </th>
-        <th class="text-left">
-          Calories
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in desserts"
-        :key="item.name"
-      >
-        <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer class="bg-shades" theme="dark" permanent>
+        <v-list color="transparent">
+          <v-list-item><h1>FoodDine<br>System</h1><span class="fw-semibold"> {{ username }}</span></v-list-item>
+          <v-divider></v-divider>
+          <v-list-item link prepend-icon="mdi-view-dashboard" title="" @click="$router.push('/admin/dashboard')"><span class="fw-semibold">Dashboard</span></v-list-item>
+          <v-list-item link prepend-icon="mdi-order-alphabetical-ascending" @click="$router.push('/admin/orders')"><span class="fw-semibold">Orders</span></v-list-item>
+          <v-list-item link prepend-icon="mdi-table" title="" @click="$router.push('/admin/tables')"><span class="fw-semibold" >Tables</span></v-list-item>
+          <v-list-item link prepend-icon="mdi-menu" title="" @click="$router.push('/admin/menus')"><span class="fw-semibold">Menus</span></v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+          <div class="mb-5 px-3">
+            <v-btn class="bg-blue-grey" block @click="logout"> Logout </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
+
+
 <script>
-  export default {
-    data () {
-      return {
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      username: this.$store.state.username
+    };
+  },
+  methods: {
+    logout() {
+      this.$store.commit('removeToken');
+      location.reload(true);
+    }
   }
+};
 </script>
+
+<style scoped>
+</style>
