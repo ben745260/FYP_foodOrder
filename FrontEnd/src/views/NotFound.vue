897b842cbfb24c1946f08670b40b4816e0968a81
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-17 16:28:29 +0800

    Update 404 page

--- FrontEnd/src/views/NotFound.vue
+++ FrontEnd/src/views/NotFound.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1 class="align-center">404 Not Found</h1>
+  <v-container class="not-found-page" fluid>
+    <v-row justify="center" :align="center" class="fill-height">
+      <v-col cols="12" class="text-center">
+        <h1 class="not-found-title">404 Not Found</h1>
+        <p class="not-found-description">The page you are looking for does not exist.</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -9,5 +16,17 @@ export default {
 </script>
 
 <style>
+.not-found-page {
+  background-color: #f5f5f5;
+}
 
+.not-found-title {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+.not-found-description {
+  font-size: 1rem;
+  color: #888;
+}
 </style>
