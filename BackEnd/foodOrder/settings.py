9ee00494941efe8f43441e9efacebcd9f86b7e55
ben745260 <106643280+ben745260@users.noreply.github.com>
2024-03-19 23:53:51 +0800

    WIP

--- BackEnd/foodOrder/settings.py
+++ BackEnd/foodOrder/settings.py
@@ -139,6 +139,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://fyp-food-order.vercel.app'
+    '*',
 ]
 # AUTH_USER_MODEL = 'shop.User'
 # Managing media
