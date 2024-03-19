b3b170e2cb2951ff6ad5bc37b3df4f27771bc96f
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-19 20:48:33 +0800

    WIP

--- BackEnd/foodOrder/settings.py
+++ BackEnd/foodOrder/settings.py
@@ -137,7 +137,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://fyp-food-order.vercel.app'
 ]
 # AUTH_USER_MODEL = 'shop.User'
 # Managing media
--- FrontEnd/src/methods/gpt/generateDescription.js
+++ FrontEnd/src/methods/gpt/generateDescription.js
@@ -1,7 +1,7 @@
 import { OpenAI } from 'openai';
 
-const apiKey = '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';  // Replace with your actual OpenAI API key
-
+// const apiKey = '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';  // Replace with your actual OpenAI API key
+const apiKey = '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';  // Replace with your actual OpenAI API key
 const openai = new OpenAI({
   apiKey,
   dangerouslyAllowBrowser: true,
@@ -26,6 +26,7 @@ export async function generateDescription(productName,systemMsg,userMsg) {
       max_tokens: 30,
       temperature: 0.7,
       stop: '\n',
+      // assistant_id: 'asst_sCP7Ij19wJkgXya8tCRj4k9V',  // Replace with your actual Assistant ID
     });
 
     const assistantResponse = response.choices[0].message.content.trim();
