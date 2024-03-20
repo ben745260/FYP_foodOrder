7bf57930326b8aea12593480b378477c6b845166
Ben <106643280+ben745260@users.noreply.github.com>
2024-03-20 22:54:46 +0800

    WIP

--- FrontEnd/src/views/Table-User.vue
+++ FrontEnd/src/views/Table-User.vue
@@ -70,7 +70,7 @@ export default {
       { name: "tablemenu", label: "Menu", to: `/table/${tableId}/tablemenu` },
       { name: "cart", label: "Cart", to: `/table/${tableId}/cart` },
       { name: "vieworder", label: "Record", to: `/table/${tableId}/vieworder` },
-      { name: "recommdenation", label: "Recommdenation", to: `/table/${tableId}/recommdenation` },
+      // { name: "recommdenation", label: "Recommdenation", to: `/table/${tableId}/recommdenation` },
       { name: "feedback", label: "Feedback", to: `/table/${tableId}/feedback` },
     ];
 
