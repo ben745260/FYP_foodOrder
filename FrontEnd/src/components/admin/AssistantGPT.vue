e09a9237ee45abb56e9b2bc269777b5dc6a57b8b
Ben <106643280+ben745260@users.noreply.github.com>
2024-04-12 10:35:40 +0800

    Update AssistantGPT.vue

--- FrontEnd/src/components/admin/AssistantGPT.vue
+++ FrontEnd/src/components/admin/AssistantGPT.vue
@@ -5,7 +5,8 @@
         <span class="fs-1">AssistantGPT </span>
       </v-card-title>
       <v-card-text>
-       <iframe class="w-100" src="https://fyp-open-ai-assistant-api-chat.vercel.app/" style="height:780px;"></iframe>
+       <!-- <iframe class="w-100" src="https://fyp-open-ai-assistant-api-chat.vercel.app/" style="height:780px;"></iframe> -->
+       <iframe class="w-100" src="https://fyp-open-ai-assistant-api-chat-gya7.vercel.app/" style="height:780px;"></iframe>
       </v-card-text>
     </v-card>
   </v-app>
